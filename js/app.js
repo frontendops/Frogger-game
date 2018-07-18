22957b1c060c6a8e16d218542e5e92043f39ee2d
Alejandro-Sierra <alexsierra1598@gmail.com>
2018-07-18 12:00:53 -0400

    enemies called and rendered, setup speed and locations

--- js/app.js
+++ js/app.js
@@ -1,21 +1,24 @@
+var allEnemies = [];
+
 // Enemies our player must avoid
-var Enemy = function(h, w, speed) {
+var Enemy = function(x, y, speed) {
     // Variables applied to each of our instances go here,
-
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
     this.sprite = 'images/enemy-bug.png';
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    speed * dt;
-    // which will ensure the game runs at the same speed for
-    // all computers.
+
+    if (this.x < 400) {
+        this.x += this.speed * dt;
+    } else {
+        this.x = -110;
+    }
+
 };
 
 // Draw the enemy on the screen, required method for game
@@ -23,14 +26,14 @@ Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+let enemy = new Enemy(-110,60,60);
+let enemy2 = new Enemy(-110,140,50);
+let enemy3 = new Enemy(-110,220,50);
 
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+allEnemies.push(enemy,enemy2,enemy3);
+
+
 
 let Player = function() {
     this.x = 205;
@@ -59,13 +62,13 @@ Player.prototype.handleInput = function(direction) {
 
         case 'up':
         if (this.y > 20) {
-            this.y -= 80;
+            this.y -= 83;
         }
         break;
 
         case 'down':
         if (this.y  < 400) {
-            this.y += 80;
+            this.y += 83;
         }
         break;
     }
